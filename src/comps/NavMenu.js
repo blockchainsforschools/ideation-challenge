c019542a5f77b23c37e1d97e3fc12437f72db1b1
Abir Taheer <abir@taheer.me>
2020-07-20 11:58:44 -0400

    added menu to mobile devices

--- src/comps/NavMenu.js
+++ src/comps/NavMenu.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
-// import { IconButton } from '@rmwc/icon-button';
+import { IconButton } from '@rmwc/icon-button';
 import { Link } from 'react-scroll';
 import '@rmwc/icon-button/styles';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
+import {
+	Drawer,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerSubtitle,
+	DrawerContent
+} from '@rmwc/drawer';
+import '@rmwc/drawer/styles';
+
+import { List, ListItem } from '@rmwc/list';
+import '@rmwc/list/styles';
 
 const desktopMin = 1180;
 
@@ -39,39 +50,99 @@ const useStyles = createUseStyles({
 	}
 });
 
+const routes = [
+	{
+		to: 'Overview',
+		text: 'Overview'
+	},
+
+	{
+		to: '/getting-started',
+		text: 'Getting Started',
+		external: true
+	},
+	{
+		to: 'ProjectSupport',
+		text: 'Project Support'
+	},
+	{
+		to: 'timelines',
+		text: 'Timelines'
+	},
+	{
+		to: 'FAQ',
+		text: 'FAQ'
+	},
+	{
+		to: 'ChallengeSponsors',
+		text: 'Challenge Sponsors'
+	},
+	{
+		to: '/CoC',
+		text: 'Code of Conduct',
+		external: true
+	}
+];
+
 const NavMenu = () => {
 	const classes = useStyles();
 	const location = useLocation();
+	const [menuOpen, setMenuOpen] = React.useState(false);
 
 	return (
 		<span className={classes.NavContainer}>
 			<span className={classes.TabbedMenu}>
 				<HomeLinks />
-
-				<RouterLink
-					className={classes.Tab}
-					to="/getting-started"
-					spy={true}
-					offset={-70}
-					duration={500}
-				>
-					Getting Started
-				</RouterLink>
-
-				<RouterLink
-					className={classes.Tab}
-					to="/CoC"
-					spy={true}
-					offset={-70}
-					duration={500}
-				>
-					Code of Conduct
-				</RouterLink>
 			</span>
 
 			<span className={classes.BurgerMenu}>
-				{/*<IconButton className={classes.BurgerIcon} icon="menu" />*/}
+				<IconButton
+					className={classes.BurgerIcon}
+					icon="menu"
+					onClick={() => setMenuOpen(a => !a)}
+				/>
 			</span>
+
+			<Drawer
+				dir="rtl"
+				modal
+				open={menuOpen && window.innerWidth < 800}
+				onClose={() => setMenuOpen(false)}
+				style={{ position: 'fixed', top: 0 }}
+			>
+				{/** Set the content back to left-to-right */}
+				<DrawerHeader dir="ltr">
+					<DrawerTitle>Blockchains Ideation Challenge</DrawerTitle>
+					<DrawerSubtitle>Blockchains For Schools</DrawerSubtitle>
+				</DrawerHeader>
+
+				<DrawerContent dir="ltr">
+					<List>
+						{routes.map(page =>
+							location.pathname === '/' && !page.external ? (
+								<Link
+									to={page.to}
+									spy={true}
+									offset={-70}
+									duration={500}
+									style={{ textDecoration: 'none' }}
+									onClick={() => setMenuOpen(false)}
+								>
+									<ListItem>{page.text}</ListItem>
+								</Link>
+							) : (
+								<RouterLink
+									to={`/#${page.to}`}
+									style={{ textDecoration: 'none' }}
+									onClick={() => setMenuOpen(false)}
+								>
+									<ListItem>{page.text}</ListItem>
+								</RouterLink>
+							)
+						)}
+					</List>
+				</DrawerContent>
+			</Drawer>
 		</span>
 	);
 };
@@ -80,48 +151,25 @@ function HomeLinks() {
 	const classes = useStyles();
 	const location = useLocation();
 
-	const routes = [
-		{
-			to: 'Overview',
-			text: 'Overview'
-		},
-		{
-			to: 'ProjectSupport',
-			text: 'Project Support'
-		},
-		{
-			to: 'timelines',
-			text: 'Timelines'
-		},
-		{
-			to: 'FAQ',
-			text: 'FAQ'
-		},
-		{
-			to: 'ChallengeSponsors',
-			text: 'Challenge Sponsors'
-		}
-	];
-
 	return (
 		<span>
-			{location.pathname === '/'
-				? routes.map(page => (
-						<Link
-							className={classes.Tab}
-							to={page.to}
-							spy={true}
-							offset={-70}
-							duration={500}
-						>
-							{page.text}
-						</Link>
-				  ))
-				: routes.map(page => (
-						<RouterLink className={classes.Tab} to="/">
-							{page.text}
-						</RouterLink>
-				  ))}
+			{routes.map(page =>
+				location.pathname === '/' && !page.external ? (
+					<Link
+						className={classes.Tab}
+						to={page.to}
+						spy={true}
+						offset={-70}
+						duration={500}
+					>
+						{page.text}
+					</Link>
+				) : (
+					<RouterLink className={classes.Tab} to={`/#${page.to}`}>
+						{page.text}
+					</RouterLink>
+				)
+			)}
 		</span>
 	);
 }
